66dc050d2c3684d53f4bd4a131306df7584f612a
jugal <jugalasahu@gmail.com>
2024-04-25 17:04:41 +0530

    Add login functionality

--- js/main.js
+++ js/main.js
@@ -11,7 +11,41 @@ if (!window.indexedDB) {
 else {
     $(function () {
         $("#register_form").on("submit", function () {
+            var check_database = window.indexedDB.databases();
+            check_database.then(function(db_list){
+                if(db_list.length==0)
+                {
+                    register();
+                }
+                else{
+                    $("#msg_box1").removeClass("d-none").addClass("alert-warning")
+                    $("#msg_box1 span").html(`
+                    <b>Registration Failed !</b> 
+                    <a href='#' style='color:blue;font-weight:bold; text-decoration:none;'>Please purchage muti version</a> 
+                    <span class="ms-4 bi bi-trash-fill" data-bs-toggle="tooltip" data-bs-placement="bottom" title="To manage another school record please delete currently used school database" id="tooltip_id"></span>
+                    `);
+                    $("#tooltip_id").tooltip();
+                    $("#tooltip_id").click(function(){
+                        $("#confirm").modal('show');
+                        $("#delete_btn").click(function(){
+                            var all_db = window.indexedDB.databases()
+                            all_db.then(function(all_db_list){
+                                var verify_delete = window.indexedDB.deleteDatabase(all_db_list[0].name);
+                                verify_delete.onsuccess = function(){
+                                    $("#register_form").trigger("reset");
+                                    $("#msg_box1").addClass("d-none");
+                                    $(".delete_modal_msg").html("");
+                                    $(".delete_success_alert").removeClass("d-none");
+                                };
+                            });
+                        });
+                    });
+                }
+            });
+            return false;
+        });
 
+        function register() {
             var school_name = $("#school_name").val();
             var tag_line = $("#tag_line").val();
             var email_id = $("#email_id").val();
@@ -23,9 +57,10 @@ else {
             // create database
             var database = window.indexedDB.open(school_name);
             database.onsuccess = function () {
+               
                 $("#msg_box").removeClass("d-none");
                 $("#msg_box").addClass("alert-success");
-                $("#msg_box").append(`<strong>Success !</strong> Dear admin please login...`);
+                $("#msg_box span").html(`<strong>Success !</strong> Dear admin please login...`);
                 $("#register_form").trigger("reset");
                 setTimeout(() => {
                     $("#msg_box").addClass("d-none");
@@ -37,7 +72,7 @@ else {
             database.onerror = function () {
                 $("#msg_box").removeClass("d-none");
                 $("#msg_box").addClass("alert-warning");
-                $("#msg_box").append(`<strong>Oops !</strong> Something went wronge...`);
+                $("#msg_box").html(`<strong>Oops !</strong> Something went wronge...`);
             }
 
             database.onupgradeneeded = function () {
@@ -55,16 +90,84 @@ else {
                 var object = idb.createObjectStore("about_school", { keyPath: "school_name" });
                 object.add(data);
             }
-
-            return false;
-        });
+        }
     });
 }
 
 // login 
 
-$(()=>{
-    $("#login_form").on("submit",function(){
-       
+$(() => {
+    $("#login_form").submit(function () {
+        var username = $("#username").val();
+        var password = $("#login_password").val();
+        var login_data = {
+            username: username,
+            password: password,
+        };
+        var json_data = JSON.stringify(login_data);
+        sessionStorage.setItem("login", json_data);
+        if (sessionStorage.getItem("login") != null) {
+            //find users from database
+            var user_database = window.indexedDB.databases();
+            user_database.then((pending_obj) => {
+                for (var item of pending_obj) {
+                    var db_name = item.name;
+                    var database = window.indexedDB.open(db_name, 1); // Specifying version as 1
+                    database.onerror = function (event) {
+                        console.error("Error opening database", event.target.error);
+                    };
+                    database.onsuccess = function (event) {
+                        var idb = event.target.result;
+                        var permission = idb.transaction("about_school", "readwrite");
+                        var access = permission.objectStore("about_school");
+                        var json_data = access.get(db_name);
+                        json_data.onsuccess = function () {
+                            var user = this.result;
+                            if (user) {
+                                var db_username = user.email_id;
+                                var db_password = user.password;
+                                var session_data = JSON.parse(sessionStorage.getItem("login"));
+                                if (session_data.username == db_username) {
+                                    if (session_data.password == db_password) {
+
+                                        window.location = "./success/welcome.html";
+                                    }
+                                    else {
+                                        $("#login_msg_box").removeClass("d-none").addClass("alert-danger")
+                                        $("#login_msg_box span").html("<b>Wronge Password </b> please try again..");
+                                        setTimeout(() => {
+                                            $("#login_msg_box").addClass("d-none");
+                                            $("#login_form").trigger("reset");
+                                        }, 2000);
+                                    }
+                                }
+                                else {
+                                    $("#login_msg_box").removeClass("d-none").addClass("alert-danger")
+                                    $("#login_msg_box span").html("<b>User Not found !</b> please try again..");
+                                    setTimeout(() => {
+                                        $("#login_msg_box").addClass("d-none");
+                                        $("#login_form").trigger("reset");
+                                    }, 2000);
+                                }
+                            }
+                            else {
+                                alert("user not found");
+                            }
+                        };
+                    };
+                }
+            });
+        }
+        else {
+
+            $("#login_msg_box").removeClass("d-none").addClass("alert-danger")
+            $("#login_msg_box span").html("<b>Session failed !</b> please try again..");
+            setTimeout(() => {
+                $("#login_msg_box").addClass("d-none");
+                $("#login_form").trigger("reset");
+            }, 2000);
+        }
+
+        return false;
     });
 });
